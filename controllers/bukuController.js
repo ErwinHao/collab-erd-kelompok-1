95d1f8a75a3368b901c55b028f8a80fe43e8af05
Erwin Hartanto <erwinhaa25@gmail.com>
2021-02-05 10:29:26 +0700

    Fix buku

--- controllers/bukuController.js
+++ controllers/bukuController.js
@@ -1,78 +1,78 @@
-const books = [{
-        "noBuku": 1,
-        "judul": "BUKU belajar",
-        "pengarang": "PT. TESTING"
-    },
-    {
-        "noBuku": 2,
-        "judul": "BUKU EXPRESS",
-        "pengarang": "JNE"
-    },
-    {
-        "noBuku": 3,
-        "judul": "BUKU SICEPAT",
-        "pengarang": "JNT"
-    },
-    {
-        "noBuku": 4,
-        "judul": "PAXEL",
-        "pengarang": "TOKOHIJAU"
-    }
-]
+const books = [
+  {
+    noBuku: 1,
+    judul: 'BUKU belajar',
+    pengarang: 'PT. TESTING',
+  },
+  {
+    noBuku: 2,
+    judul: 'BUKU EXPRESS',
+    pengarang: 'JNE',
+  },
+  {
+    noBuku: 3,
+    judul: 'BUKU SICEPAT',
+    pengarang: 'JNT',
+  },
+  {
+    noBuku: 4,
+    judul: 'PAXEL',
+    pengarang: 'TOKOHIJAU',
+  },
+];
 
 exports.getBuku = (req, res) => {
-    return res.status(200).json({
-        success: true,
-        books,
-    });
+  return res.status(200).json({
+    success: true,
+    books,
+  });
 };
 
-
 exports.postBuku = (req, res) => {
-    const judul = req.body.judul;
-    const pengarang = req.body.pengarang;
-    let data = books[books.length - 1].books + 1;
+  const judul = req.body.judul;
+  const pengarang = req.body.pengarang;
+  let data = books[books.length - 1].books + 1;
 
-    const params = {
-        noBuku: data,
-        judul,
-        pengarang,
-    };
+  const params = {
+    noBuku: data,
+    judul,
+    pengarang,
+  };
 
-    books.push(params);
+  books.push(params);
 
-    return res.status(201).json({
-        success: true,
-        params,
-    });
+  return res.status(201).json({
+    success: true,
+    params,
+  });
 };
 
 exports.putBuku = (req, res) => {
-    const judul = req.body.judul;
-    const pengarang = req.body.pengarang;
+  const judul = req.body.judul;
+  const pengarang = req.body.pengarang;
 
-    const params = { judul: newJudul, pengarang: newPengarang };
+  const params = { judul, pengarang };
 
-    const noBuku = req.params.nim;
+  const noBuku = req.params.nim;
 
-    const fetchedBooks = books.find((i) => i.noBuku === noBuku);
+  const fetchedBooks = books.find((i) => i.noBuku === noBuku);
 
-    const updated = {...fetchedBooks, ...params };
+  const updated = { ...fetchedBooks, ...params };
 
-    return res.status(200).json({
-        success: true,
-        updated,
-    });
+  return res.status(200).json({
+    success: true,
+    updated,
+  });
 };
 
 exports.deleteBuku = (req, res) => {
-    const no = +req.params.books;
+  const no = +req.params.noBuku;
 
-    const deleted = books.filter((buku) => buku.no === no);
+  const deleted = books.filter((buku) => buku.noBuku === no);
 
-    return res.status(200).json({
-        success: true,
-        message: `Deleted ${deleted[0].judul} from data!`,
-        deleted,
-    });
+  return res.status(200).json({
+    success: true,
+    message: `Deleted ${deleted[0].judul} from data!`,
+    deleted,
+  });
 };
--- routes/buku.js
+++ routes/buku.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const {
-    getBuku,
-    postBuku,
-    putBuku,
-    deleteBuku,
+  getBuku,
+  postBuku,
+  putBuku,
+  deleteBuku,
 } = require('../controllers/bukuController');
 
 router.get('/buku', getBuku);
 router.post('/buku', postBuku);
-router.put('/buku/:noBuku', putMahasiswa);
-router.delete('/mahasiswa/:noBuku', deleteMahasiswa);
+router.put('/buku/:noBuku', putBuku);
+router.delete('/buku/:noBuku', deleteBuku);
 
 module.exports = router;
