07376095476ba1b8b8a5c744b0c424cca9d791a9
vamosina <yoppier87@gmail.com>
2021-02-05 10:14:30 +0700

    [YRW] BUKU

---
+++ routes/buku.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+    getBuku,
+    postBuku,
+    // putBuku,
+    // deleteBuku,
+} = require('../controllers/bukuController');
+
+router.get('/buku', getBuku);
+router.post('/buku', postBuku);
+// router.put('/buku/:noBuku', putMahasiswa);
+// router.delete('/mahasiswa/:noBuku', deleteMahasiswa);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const express = require('express');
 const app = express();
 
 const mhsRoutes = require('./routes/mahasiswa');
+const bukuRoutes = require('./routes/buku');
 
 app.use(express.json());
 
 app.use(mhsRoutes);
-
+app.use(bukuRoutes);
 app.listen(3000);
